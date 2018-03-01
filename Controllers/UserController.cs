523948bda9a3220121d13cf19ab77853fcf3188e
The-Vampiire <vampiirecodes@gmail.com>
2018-02-28 23:30:21 -0500

    basic dotnet MVC app for User Signup studio

---
+++ Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace user_signup.Controllers {
+    public class UserController : Controller {
+        // GET
+        public IActionResult Index() {
+            return
+            View();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace user_signup.Models {
+    public class User {
+    // PRIVATE
+        
+        // FIELDS
+        private string username;
+        private string password;
+        private string email;
+        
+        // PROPERTIES
+        private string Password {
+            get => password;
+            set {
+                if (password.Length > 20) {
+                    throw new ArgumentException("Password is too long. Must be less than 20 characters");
+                }
+
+                if (password.Length < 5) {
+                    throw new ArgumentException("Password is too short. Must be at least 5 characters");
+                }
+                password = value;
+            }
+        }
+
+        // METHODS
+        
+    // PUBLIC
+        
+        // PROPERTIES
+        public string Username {
+            get => username;
+            set {
+                if (value.Length > 15) {
+                    throw new ArgumentException("Username is too long. Must be less than 15 characters");
+                }
+                if (value.Length < 5) {
+                    throw new ArgumentException("Username is too short. Must be more than 5 characters");
+                }
+                username = value;
+            }
+        }
+        
+        public string Email {
+            get => email;
+            set => email = value;
+        }
+        
+        // METHODS
+        
+        public bool CheckPassword(string formPassword) => Password.Equals(formPassword);  
+    
+        
+
+    // CONSTRUCTORS
+        public User() {
+           // empty constructor 
+        }
+        
+        public User(string username, string email, string password) {
+            Username = username;
+            Email = email;
+            Password = password;
+        }
+        
+    }
+}
